d4315cb74177abd0bbdd1da5e56155c8160a1018
Shirin <mirshirin8353@gmail.com>
2024-09-12 15:44:55 +0330

    Update .gitignore and remove tracked files

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('product', [ProductController::class,'index'])->name('product.index');
